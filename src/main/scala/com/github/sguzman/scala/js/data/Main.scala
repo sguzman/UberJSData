596214b7b9a44184918ff487c7ff027355efd304
Salvador Guzman <guzmansalv@gmail.com>
2018-02-28 23:11:19 -0800

    construct js file with items var

--- src/main/scala/com/github/sguzman/scala/js/data/Main.scala
+++ src/main/scala/com/github/sguzman/scala/js/data/Main.scala
@@ -9,6 +9,7 @@ import com.github.sguzman.scala.js.data.typesafe.data.trip.Trip
 import com.github.sguzman.scala.js.data.typesafe.verify.PlatformChromeNavData
 import io.circe.generic.auto._
 import io.circe.parser.decode
+import io.circe.syntax._
 
 import scala.util.{Failure, Success}
 import scalaj.http.{Http, HttpResponse}
@@ -17,7 +18,8 @@ object Main {
   val cookie: String = System.getenv("COOKIE")
 
   implicit final class StrWrap(str: String) {
-    def req = Http(str).header("Cookie", cookie)
+    def cons = s"https://partners.uber.com/p3/$str"
+    def req = Http(cons).header("Cookie", cookie)
     def exe: HttpResponse[String] = util.Try(req.asString) match {
       case Success(v) => v
       case Failure(e) =>
@@ -33,33 +35,36 @@ object Main {
     }
 
     def statements = {
-      "https://partners.uber.com/p3/platform_chrome_nav_data".verify()
-      decode[List[AllDataStatement]](str.body)
-        .right
-        .get
+      "platform_chrome_nav_data".verify()
+      decode[List[AllDataStatement]](str.body).right.get
     }
   }
 
   implicit final class UUIDWrap(uuid: UUID) {
-    def statement = decode[Statement](s"https://partners.uber.com/p3/money/statements/view/${uuid.toString}".body) match {
+    def statement = decode[Statement](s"money/statements/view/${uuid.toString}".body) match {
       case Right(v) => v
       case Left(e) => throw e
     }
 
-    def trip = decode[Trip](s"https://partners.uber.com/p3/money/trips/trip_data/${uuid.toString}") match {
+    def trip = decode[Trip](s"money/trips/trip_data/${uuid.toString}".body) match {
       case Right(v) => v
       case Left(e) => throw e
     }
   }
 
-  def main(args: Array[String]): Unit = {
-    "https://partners.uber.com/p3/money/statements/all_data/"
-      .statements
-      .par
-      .map(_.uuid)
-      .map(_.statement)
-      .flatMap(_.body.driver.trip_earnings.trips.keys)
-      .map(_.trip) foreach println
-  }
+  def items = "money/statements/all_data/"
+    .statements
+    .par
+    .map(_.uuid)
+    .map(_.statement)
+    .flatMap(_.body.driver.trip_earnings.trips.keys)
+    .map(_.trip)
+    .toList
+    .asJson
+    .toString
+
+  def js = "var items = " ++ items ++ ";"
+
+  def main(args: Array[String]): Unit = println(js)
 
 }
