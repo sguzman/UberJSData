e6518f4d2b11e19b6f88725c07a591d9740f19fe
Salvador Guzman <guzmansalv@gmail.com>
2018-02-28 16:33:17 -0800

    remove custom data

--- src/main/scala/com/github/sguzman/scala/js/data/trip/Loc.scala
+++
@@ -1,7 +0,0 @@
-package com.github.sguzman.scala.js.data.trip
-
-case class Loc(
-                pick: Pinpoint,
-                drop: Pinpoint,
-                trip: Trip
-              )
--- src/main/scala/com/github/sguzman/scala/js/data/trip/Pinpoint.scala
+++
@@ -1,8 +0,0 @@
-package com.github.sguzman.scala.js.data.trip
-
-case class Pinpoint(
-                 lat: Double,
-                 lng: Double
-                 ) {
-  def latlng = new google.maps.LatLng(lat, lng)
-}
