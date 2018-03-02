5b3255b3062fd2f5a946e2bc02cec3316510ea6f
Salvador Guzman <guzmansalv@gmail.com>
2018-03-01 19:24:14 -0800

    use .par for concurrency

--- src/main/scala/com/github/sguzman/scala/js/data/Main.scala
+++ src/main/scala/com/github/sguzman/scala/js/data/Main.scala
@@ -1,8 +1,39 @@
 package com.github.sguzman.scala.js.data
 
-object Main {
-  def main(args: Array[String]): Unit = {
+import lol.http._
 
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Failure, Success}
+
+object Main {
+  var js = "..."
+
+  def dl = {
+    def download(idx: Int = 0): Unit = {
+      println(s"Downloading $idx")
+      js = Items.js
+      println(s"Finished $idx")
+      Thread.sleep(86400000)
+      download(idx + 1)
+    }
+
+    download()
   }
 
+  def server(port: Int) = Server.listen(port) {
+    case GET at "/js" =>
+      Ok(js)
+    case _ =>
+      NotFound
+  }
+
+  def start = server(util.Try(System.getenv("PORT").toInt) match {
+    case Success(v) => v
+    case Failure(_) => 8080
+  })
+
+  def main(args: Array[String]): Unit =
+    List[() => Unit](dl _, start _)
+      .par
+      .foreach(t => t.apply)
 }
